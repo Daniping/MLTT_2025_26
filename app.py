ab3507cddb5581c7dd806801257d2fc15d62ff3f
Daniping <daniping95@gmail.com>
2025-08-29 01:00:53 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,3 +3,20 @@ from ics import Calendar, Event
 from datetime import datetime, timedelta
 
 app = Flask(__name__)
+
+matches = [
+    ("2025-09-05 16:00", "Florida Crocs", "Portland Paddlers", "Pleasanton, CA"),
+    ("2025-09-05 19:30", "Texas Smash", "Bay Area Blasters", "Pleasanton, CA"),
+    ("2025-09-06 16:00", "Florida Crocs", "Texas Smash", "Pleasanton, CA"),
+    ("2025-09-06 19:30", "Portland Paddlers", "Bay Area Blasters", "Pleasanton, CA"),
+    ("2025-09-07 11:00", "Portland Paddlers", "Texas Smash", "Pleasanton, CA"),
+    ("2025-09-07 14:30", "Florida Crocs", "Bay Area Blasters", "Pleasanton, CA"),
+    ("2025-09-19 19:30", "Atlanta Blazers", "Carolina Gold Rush", "Charlotte, NC"),
+    ("2025-09-20 19:30", "Florida Crocs", "Carolina Gold Rush", "Charlotte, NC"),
+    ("2025-09-21 14:30", "New York Slice", "Carolina Gold Rush", "Charlotte, NC"),
+    ("2025-10-03 19:30", "Los Angeles Spinners", "Portland Paddlers", "Portland, OR"),
+    ("2025-10-04 16:00", "Carolina Gold Rush", "Los Angeles Spinners", "Portland, OR"),
+    ("2025-10-04 19:30", "Chicago Wind", "Portland Paddlers", "Portland, OR"),
+    ("2025-10-05 14:30", "Carolina Gold Rush", "Portland Paddlers", "Portland, OR"),
+    # … ajouter toutes les autres semaines de la même manière …
+]

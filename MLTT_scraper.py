be34983e345884b43eff0c137265dee00f6da88b
Daniping <daniping95@gmail.com>
2025-09-07 23:55:56 +0200

    Update MLTT_scraper.py

--- MLTT_scraper.py
+++ MLTT_scraper.py
@@ -1,6 +1,6 @@
 OUTPUT_FILE = "MLTT_2025_26_V5.ics"
 
 with open(OUTPUT_FILE, "a", encoding="utf-8") as f:
-    f.write("bonjour\n")
+    f.write("bonjour et va te faire... \n")
 
 print(f"[OK] 'bonjour' écrit dans {OUTPUT_FILE}") 
